7fe971cb6270d96835fb1fb69956282250b21407
elodieemiliel1-png <elodieemiliel1@gmail.com>
2026-02-15 02:33:53 +0100

    Correction Template ID EmailJS - utilisation du vrai Template ID

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import emailjs from '@emailjs/browser';
 
 // Configuration EmailJS
 const EMAILJS_SERVICE_ID = 'service_miikrwb';
-const EMAILJS_TEMPLATE_ID = 'yb1h4xo';
+const EMAILJS_TEMPLATE_ID = 'template_9v371ts';
 const EMAILJS_PUBLIC_KEY = 'I1ieEJYMgLISGsRaz';
 
 export function Contact() {
